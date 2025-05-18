1da021a7024ce45362bad76a29fe80ea0d22dec3
Ghazi Nakkash <ghazi.nakkash@student.htw-Berlin.de>
2025-05-18 18:35:08 +0200

    test with static main

--- app/main.py
+++ app/main.py
@@ -7,12 +7,21 @@ logging.basicConfig(level=logging.INFO)
 async def main():
     try:
         prompt = "this is a test. just say hey!"
+
         client, model = get_openai_client()
-        response = get_response(prompt, client, model)
-        #response = "This is a mock response for testing."
+        #response = get_response(prompt, client, model)
+        response = "This is a mock response for testing."
         logging.info(f"OpenAI response: {response}")
     except Exception as e:
         logging.error(f"Error in main process: {e}")
+
+
+
+def build_prompt(prompt: str, semgrep_report: str) -> str:
+    """
+    Builds the prompt for OpenAI API.
+    """
+    return f"{prompt}\n{semgrep_report}"
 
 if __name__ == "__main__":
     asyncio.run(main())
